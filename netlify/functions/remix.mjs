74a43b441380141edd7a28d23da64b13a76738bd
Darshan Godase <darshangodase10@gmail.com>
2025-05-31 15:03:17 +0530

    changed file

--- netlify/functions/remix.mjs
+++ netlify/functions/remix.mjs
@@ -3,6 +3,14 @@ import { installGlobals } from "@remix-run/node";
 
 installGlobals();
 
+let build;
+try {
+  build = await import("../../build/server/index.js");
+} catch (error) {
+  console.error("Failed to import build:", error);
+  throw error;
+}
+
 export const handler = createRequestHandler({
-  build: await import("../../build/server/index.js"),
+  build,
 }); 
