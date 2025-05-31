9a347a11d52fd9dcfa4b0c0b098c70f598f4f08c
Darshan Godase <darshangodase10@gmail.com>
2025-05-31 11:26:35 +0530

    changed vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-// ↓ add this
-import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 export default defineConfig({
   plugins: [
     remix(),
     tsconfigPaths(),
-    netlifyPlugin() 
   ]
 });
