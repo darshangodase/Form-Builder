de4a7c2b9a28731ddb825f229ac89f95bf9f6a73
Darshan Godase <darshangodase10@gmail.com>
2025-05-31 10:25:00 +0530

    added new files

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,5 +1,6 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import { persistStore, persistReducer } from 'redux-persist';
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
 import authReducer from './slices/authSlice';
 
