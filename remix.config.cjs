9849fa6445df21ec45e87c365513ed5bca24f1b5
Darshan Godase <darshangodase10@gmail.com>
2025-05-31 11:55:33 +0530

    fixed

--- remix.config.cjs
+++ remix.config.cjs
@@ -1,9 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-    appDirectory: "app",
-    assetsBuildDirectory: "public/build",
-    publicPath: "/build/",
-    serverBuildTarget: "netlify",
-    serverModuleFormat: "cjs",
-  };
-  
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildTarget: "netlify",
+  serverModuleFormat: "cjs",
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
+import { vitePlugin as remix } from "@remix-run/dev";
 
 export default defineConfig({
-  plugins: [
-    remix(),
-    tsconfigPaths(),
-    netlifyPlugin()
-  ]
+  plugins: [remix(), tsconfigPaths(), netlifyPlugin()]
 });
